5d1556220723b98aa39ec3ca364c664d023f54cc
Daniel Pope <mauve@mauveweb.co.uk>
2017-05-24 10:37:17 -0700

    Bump version: 0.2.0 → 0.3.0

--- flake8_html/__init__.py
+++ flake8_html/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Daniel Pope"""
 __email__ = 'mauve@mauveweb.co.uk'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 
 from .plugin import HTMLPlugin
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='flake8-html',
-    version='0.2.0',
+    version='0.3.0',
     description="Generate HTML reports of flake8 violations",
     long_description=readme + '\n\n' + history,
     author="Daniel Pope",
